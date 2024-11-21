9d65cf7011fa79af752500e300193fb1eceab2f3
Ivayla Dimitrova <124716@students.ue-varna.bg>
2024-11-21 12:19:12 +0200

    debug

--- zadachi1i2.cpp
+++ zadachi1i2.cpp
@@ -38,20 +38,27 @@ int main()
     while (true) { //предотвраява програмата от затваряне след избрана функция
         cout << "Select a function by typing its number:\n 1. Reverse the string\n 2. Check if palindrome\n 3. Exit\n";
         cin >> choice;
-        if (choice == 1) {
-            revString();
+        try {
+            if (choice == 1) {
+                revString();
+            }
+            else if (choice == 2) {
+                isPalindrome();
+            }
+            else if (choice == 3) {
+                break;
+            }
+            else {
+                cout << "Invalid choice. Please try again." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // предотвратява безкраен loop при въведен грешен тип данни
+            }
         }
-        else if (choice == 2) {
-            isPalindrome();
+        catch (exception& e) {
+            cerr << "Error: " << e.what() << endl;
         }
-        else if (choice == 3) {
-            break;
-        }
-        else {
-            cout << "Invalid choice. Please try again." << endl;
-            continue;
-        }
+        
     }
     return 0;
-
+    
 }
